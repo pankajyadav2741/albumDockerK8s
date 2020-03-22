5a8a5dcc37ca36b315b4dff458516b99a7e0676c
Pankaj Yadav <pankajyadav2741@gmail.com>
2020-03-23 00:15:53 +0530

    Update main.go
    1. Reading ENV from album_deployment.yaml regarding DB_HOST where Cassandra DB will be present so that the album POD can talk to cassandra POD via the service.
    2. Automated creation of keyspace and DB table.

--- main.go
+++ main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gocql/gocql"
-	"github.com/gorilla/mux"
-	//"io/ioutil"
 	"log"
 	"net/http"
+	"github.com/gocql/gocql"
+	"github.com/gorilla/mux"
+	"github.com/kelseyhightower/envconfig"
 )
 
 type Albums struct {
-	Name string `json:"name"`
+	Name  string  `json:"name"`
 	Image []Image `json:"image"`
 }
 
@@ -23,33 +23,47 @@ var albums []Albums
 var Session *gocql.Session
 
 func init() {
-	var err error
-	cluster := gocql.NewCluster("127.0.0.1")
+	var host string
+	err := envconfig.Process("DB_HOST", host)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	cluster := gocql.NewCluster(host)
 	cluster.Keyspace = "albumspace"
-	Session, err = cluster.CreateSession()
+	Session, err := cluster.CreateSession()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Cassandra init done")
 
-	//TODO: Create Keyspace
+	//Create Keyspace
+	if err := Session.Query(`CREATE KEYSPACE ? IF NOT EXISTS WITH replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };`, cluster.Keyspace).Exec(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Fprintf(w, "")
+	}
 
-	//USE cluster.Keyspace
-	//TODO: Create TYPE
-	//CREATE TYPE IF NOT EXISTS test.image3 ( imgname text);
+	//Use Keyspace
+	if err := Session.Query(`USE ?;`, cluster.Keyspace).Exec(); err != nil {
+		fmt.Println(err)
+	}
 
-	//TODO: Create Table
-	//CREATE TABLE IF NOT EXISTS test.album3 (albname text PRIMARY KEY, images list<FROZEN <image3>>);
-
+	//Create Table albumtable
+	if err := Session.Query(`CREATE TABLE IF NOT EXISTS albumtable(albname TEXT PRIMARY KEY, imagelist LIST<TEXT>);`).Exec(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Fprintf(w, "Table albumtable created")
+	}
 }
 
 //Show all albums
-func showAlbum(w http.ResponseWriter, r *http.Request){
+func showAlbum(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Displaying album names:\n")
 	//CQL Operation
-	iter:=Session.Query("SELECT albname FROM albumtable;").Iter()
+	iter := Session.Query("SELECT albname FROM albumtable;").Iter()
 	var data string
-	for iter.Scan(&data){
+	for iter.Scan(&data) {
 		json.NewEncoder(w).Encode(data)
 	}
 	if err := iter.Close(); err != nil {
@@ -58,10 +72,10 @@ func showAlbum(w http.ResponseWriter, r *http.Request){
 }
 
 //Create a new album
-func addAlbum(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","application/json")
+func addAlbum(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	param := mux.Vars(r)
-	if err:= Session.Query(`INSERT INTO albumtable (albname) VALUES (?) IF NOT EXISTS;`,param["album"]).Exec();err!=nil {
+	if err := Session.Query(`INSERT INTO albumtable (albname) VALUES (?) IF NOT EXISTS;`, param["album"]).Exec(); err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Fprintf(w, "New album added")
@@ -70,10 +84,10 @@ func addAlbum(w http.ResponseWriter, r *http.Request){
 
 //Delete an existing album
 func deleteAlbum(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
 	param := mux.Vars(r)
 	//CQL Operation
-	if err:= Session.Query(`DELETE FROM albumtable WHERE albname=? IF EXISTS;`,param["album"]).Exec();err!=nil {
+	if err := Session.Query(`DELETE FROM albumtable WHERE albname=? IF EXISTS;`, param["album"]).Exec(); err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Fprintf(w, "Album deleted")
@@ -82,11 +96,11 @@ func deleteAlbum(w http.ResponseWriter, r *http.Request) {
 
 //Show all images in an album
 func showImagesInAlbum(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
 	param := mux.Vars(r)
-	iter:=Session.Query("SELECT imagelist FROM albumtable WHERE albname=?;",param["album"]).Iter()
+	iter := Session.Query("SELECT imagelist FROM albumtable WHERE albname=?;", param["album"]).Iter()
 	var data []string
-	for iter.Scan(&data){
+	for iter.Scan(&data) {
 		json.NewEncoder(w).Encode(data)
 	}
 	if err := iter.Close(); err != nil {
@@ -96,11 +110,11 @@ func showImagesInAlbum(w http.ResponseWriter, r *http.Request) {
 
 //Show a particular image inside an album
 func showImage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
 	param := mux.Vars(r)
-	iter:=Session.Query("SELECT imagelist FROM albumtable WHERE albname='?';",param["image"]).Iter()
+	iter := Session.Query("SELECT imagelist FROM albumtable WHERE albname='?';", param["image"]).Iter()
 	var data []string
-	for iter.Scan(&data){
+	for iter.Scan(&data) {
 		for _, img := range data {
 			if img == param["image"] {
 				json.NewEncoder(w).Encode(img)
@@ -114,10 +128,10 @@ func showImage(w http.ResponseWriter, r *http.Request) {
 
 //Create an image in an album
 func addImage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
 	param := mux.Vars(r)
 	//CQL Operation
-	if err:= Session.Query(`UPDATE albumtable SET imagelist=imagelist+['?'] WHERE albname=?;`,param["image"],param["album"]).Exec();err!=nil {
+	if err := Session.Query(`UPDATE albumtable SET imagelist=imagelist+['?'] WHERE albname=?;`, param["image"], param["album"]).Exec(); err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Fprintf(w, "New image added")
@@ -126,13 +140,13 @@ func addImage(w http.ResponseWriter, r *http.Request) {
 
 //Delete an image in an album
 func deleteImage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
 	param := mux.Vars(r)
 	//CQL Operation
-	if err:= Session.Query(`UPDATE albumtable SET imagelist=imagelist-['?'] WHERE albname=?;`,param["image"],param["album"]).Exec();err!=nil {
+	if err := Session.Query(`UPDATE albumtable SET imagelist=imagelist-['?'] WHERE albname=?;`, param["image"], param["album"]).Exec(); err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Fprintf(w, "New image added")
+		fmt.Fprintf(w, "Image deleted")
 	}
 }
 
@@ -141,19 +155,19 @@ func main() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 
 	//Show all albums
-	myRouter.HandleFunc("/",showAlbum).Methods(http.MethodGet)
+	myRouter.HandleFunc("/", showAlbum).Methods(http.MethodGet)
 	//Create a new album
-	myRouter.HandleFunc("/{album}",addAlbum).Methods(http.MethodPost)
+	myRouter.HandleFunc("/{album}", addAlbum).Methods(http.MethodPost)
 	//Delete an existing album
-	myRouter.HandleFunc("/{album}",deleteAlbum).Methods(http.MethodDelete)
+	myRouter.HandleFunc("/{album}", deleteAlbum).Methods(http.MethodDelete)
 
 	//Show all images in an album
-	myRouter.HandleFunc("/{album}",showImagesInAlbum).Methods(http.MethodGet)
+	myRouter.HandleFunc("/{album}", showImagesInAlbum).Methods(http.MethodGet)
 	//Show a particular image inside an album
-	myRouter.HandleFunc("/{album}/{image}",showImage).Methods(http.MethodGet)
+	myRouter.HandleFunc("/{album}/{image}", showImage).Methods(http.MethodGet)
 	//Create an image in an album
-	myRouter.HandleFunc("/{album}/{image}",addImage).Methods(http.MethodPost)
+	myRouter.HandleFunc("/{album}/{image}", addImage).Methods(http.MethodPost)
 	//Delete an image in an album
-	myRouter.HandleFunc("/{album}/{image}",deleteImage).Methods(http.MethodDelete)
-	log.Fatal(http.ListenAndServe(":8085",myRouter))
+	myRouter.HandleFunc("/{album}/{image}", deleteImage).Methods(http.MethodDelete)
+	log.Fatal(http.ListenAndServe(":8085", myRouter))
 }
